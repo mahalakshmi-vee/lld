e13ef92aea7144fd92fe0daf4efe17615c9792d8
mahalakshmi-vee <mahalakshmi.veeraraj.scaler@gmail.com>
2024-09-22 23:49:53 +0530

    Add files via upload

---
+++ design-patterns/creational-design-pattern/object-pool/DBConnection.java
@@ -0,0 +1,11 @@
+package pac;
+
+import java.sql.SQLException;
+
+public class DBConnection {
+	// Connection mysqlConnection;
+
+	public DBConnection() throws SQLException {
+		// mysqlConnection = DriverManager.getConnection("url", "username", "password");
+	}
+}
