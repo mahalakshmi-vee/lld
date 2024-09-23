db0bfcb0056075379104b3b0636544485819af65
mahalakshmi-vee <mahalakshmi.veeraraj.scaler@gmail.com>
2024-09-23 11:40:12 +0530

    Add files via upload

---
+++ design-patterns/structural-design-pattern/decorator/BasePizza.java
@@ -0,0 +1,5 @@
+package pac;
+
+public abstract class BasePizza {
+	public abstract int cost();
+}
---
+++ design-patterns/structural-design-pattern/decorator/Mushroom.java
@@ -0,0 +1,14 @@
+package pac;
+
+public class Mushroom extends ToppingDecorator {
+	private BasePizza basePizza;
+
+	public Mushroom(BasePizza basePizza) {
+		this.basePizza = basePizza;
+	}
+
+	@Override
+	public int cost() {
+		return basePizza.cost() + 50;
+	}
+}
